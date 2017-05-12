552fd3df56eacf73ae29726787d42e4b72d86255
MarivaldoRosa <marivaldoemaykon@gmail.com>
2017-05-12 19:53:12 -0300

    Update ExemploGitNetbeans.java

--- src/exemplogitnetbeans/ExemploGitNetbeans.java
+++ src/exemplogitnetbeans/ExemploGitNetbeans.java
@@ -5,6 +5,7 @@ public class ExemploGitNetbeans {
 
     public static void main(String[] args) {
         System.out.println("Exemplo de projeto com git e Netbeans");
+        //Alterando dados na web
     }
     
 }
